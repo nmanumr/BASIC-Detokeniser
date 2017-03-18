59e6c1b8d09bfd58766c04e4e599b7e9f777bbab
Nauman Umer <u.nauman@outlook.com>
2017-03-18 07:48:02 +0500

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var decoder_1 = require("./converter/decoder");
-var fs = require('fs');
-var buffer = fs.readFileSync('C:\\Users\\Nauman Umer\\C.BAS');
-var _decoder = new decoder_1.decoder();
-_decoder.setStream(buffer);
-console.log(_decoder.decode());
